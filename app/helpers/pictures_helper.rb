58e36e220d06d69a26a2fdd851c13281b34af9c0
Christopher de Alba <superototus@gmail.com>
2013-09-12 23:11:03 -0700

    First up.

--- app/helpers/pictures_helper.rb
+++
@@ -1,5 +0,0 @@
-module PicturesHelper
-  def can_edit_picture?(picture)
-    signed_in? && current_user == picture.user
-  end
-end
--- app/models/album.rb
+++
@@ -1,5 +0,0 @@
-class Album < ActiveRecord::Base
-  belongs_to :user
-  has_many :pictures
-  attr_accessible :title
-end
